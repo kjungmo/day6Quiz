eb6395811801ea7ab8a59d32d1a1d5da714b73a3
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-07 01:38:22 +0900

    every

--- src/index.js
+++ src/index.js
@@ -2,10 +2,38 @@
 import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 
-const countryChoose = document.querySelector(".countryChoice");
+/*
+var nara = document.getElementById("userCountry");
+var User = nara.options[nara.selectedIndex].value;
+*/
 
+let select = document.querySelector("#userCountry");
+
+const userC = "country";
+
+function saveC() {
+  localStorage.setItem(userC, select.value);
+}
+
+function getC() {
+  select.addEventListener("change", saveC);
+}
+
+/*
+function loadC(){
+  const selected = localStorage.getItem("country");
+  if(selected){
+    const option = document.querySelector(`option[value="${selected}"]`);
+    option.selected = true;
+  }
+}
+function handleChange() {
+  const selected = select.value;
+  localStorage.setItem("country", selected);
+}
+*/
 function init() {
-  console.log(countryChoose);
+  getC();
 }
 
 init();
